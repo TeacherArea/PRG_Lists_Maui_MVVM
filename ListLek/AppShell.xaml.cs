9556b28d7ce181219ff4a89e9f9988eb15f5e1ca
TeacherArea <sewilius.berg@initialize.se>
2025-03-20 10:15:15 +0100

    first commit

---
+++ ListLek/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ListLek
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ListLek/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Collections.ObjectModel;
+
+namespace ListLek
+{
+    public class Person
+    {
+        public string Name { get; set ; }
+        public int Age { get; set; }
+        public string Password { get; set; }
+    }
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<Person> myList { get; set; } = new()
+        {
+            new Person { Name = "Stina", Age = 12, Password = "Hemligt" },
+            new Person { Name = "Pelle", Age = 14, Password = "Hemligt" }
+        };
+         
+
+        public MainPage()
+        {
+           
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+
+    }
+
+}
